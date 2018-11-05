d323cf75d35059e3a9ed9319a48d27795ec85f73
jooho812 <kasin812@jam2in.com>
2018-11-05 18:28:14 +0900

    zabbix : add port in init.bat

--- zabbix/init.bat
+++ zabbix/init.bat
@@ -8,6 +8,8 @@ SET dir=%cd%
 SET ip=192.168.1.172
 :: #HOSTNAME
 SET hostname=LC_01
+:: #LISTENPORT (server port)
+SET port=10050
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: make config file(%cd%\conf)
@@ -23,6 +25,7 @@ copy %dir%\conf\example\zabbix_agentd.win.example.conf %dir%\conf\zabbix_agentd.
 call :REPLACE_STRING conf\zabbix_agentd.win.conf #DIRECTORY_PATH %dir%
 call :REPLACE_STRING conf\zabbix_agentd.win.conf #ZABBIX_SERVER_IP %ip%
 call :REPLACE_STRING conf\zabbix_agentd.win.conf #HOSTNAME %hostname%
+call :REPLACE_STRING conf\zabbix_agentd.win.conf #LISTENPORT %port%
 
 :: replace userparameter_ndream.conf(only replace directory_path)
 call :REPLACE_STRING conf\userparameter_ndream.conf #DIRECTORY_PATH %dir%
