ce4f14ba7f099db8b16db2128545a314b47adebd
jooho812 <kasin812@jam2in.com>
2018-11-12 16:31:10 +0900

    zabbix : add memory, disk scripts

---
+++ zabbix/scripts/ndream_memory_counter.ps1
@@ -0,0 +1,21 @@
+﻿# Get Memory Information
+$memorySize = Get-WMIObject Win32_PhysicalMemory | Measure -Property capacity -Sum | %{$_.sum/1Mb}
+$memoryAvail = (Get-Counter '\Memory\Available MBytes').CounterSamples.CookedValue
+
+$memoryUsage = [math]::Round([double] $memoryAvail / [double] $memorySize * 100, 2)
+
+
+write-host "{"
+write-host " `"data`":[`n"
+
+    $line= "{ `"{#MEM_AVAIL}`" : `"" + $memoryAvail + "`",
+              `"{#MEM_SIZE}`" : `"" + $memorySize + "`",
+              `"{#MEM_USAGE}`" : `"" + $memoryUsage + "`"}"
+
+    write-host $line
+        
+# Write our closing JSON brackets
+ write-host
+write-host " ]"
+write-host "}"
+ 
