397fda8c489a41285e5772b3711907808952a658
jooho812 <kasin812@jam2in.com>
2018-11-13 16:45:04 +0900

    zabbix : modified zabbix template

--- zabbix/scripts/ndream_disk_avail_size.ps1
+++ zabbix/scripts/ndream_disk_avail_size.ps1
@@ -4,6 +4,6 @@ $diskInfo=wmic logicaldisk get size,freespace,caption | findstr $args[0]
 $caption,[string]$diskFree,[string]$diskSize=$diskInfo -split '\s+'
 $diskSize = $diskSize.Substring(0,$diskSize.length-1)
 [double] $diskUsed = [double]$diskSize - [double]$diskFree
-$diskFreeGB = $diskFree / (1024 * 1024)
+$diskFreeGB = [math]::Round([double] $diskFree / ([double] (1024 * 1024)),2)
 
 write-host -NoNewline $diskFreeGB
