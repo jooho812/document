a771c72bf50f652dbe2737316af32cedbab7da5c
jooho812 <kasin812@jam2in.com>
2018-11-13 15:06:38 +0900

    zabbix : add userparameter(disk avail space)

---
+++ zabbix/scripts/ndream_disk_avail_size.ps1
@@ -0,0 +1,9 @@
+﻿
+# Get Disk Information
+$diskInfo=wmic logicaldisk get size,freespace,caption | findstr $args[0]
+$caption,[string]$diskFree,[string]$diskSize=$diskInfo -split '\s+'
+$diskSize = $diskSize.Substring(0,$diskSize.length-1)
+[double] $diskUsed = [double]$diskSize - [double]$diskFree
+$diskFreeGB = $diskFree / (1024 * 1024)
+
+write-host -NoNewline $diskFreeGB
---
+++ zabbix/scripts/ndream_disk_usage.ps1
@@ -0,0 +1,26 @@
+﻿
+# Get Disk Information
+$diskInfo=wmic logicaldisk get size,freespace,caption | findstr $args[0]
+#$diskInfo=$diskInfo -replace "`n|`r|`t",""
+$caption,[string]$diskFree,[string]$diskSize=$diskInfo -split '\s+'
+$diskSize = $diskSize.Substring(0,$diskSize.length-1)
+[double] $diskUsed = [double]$diskSize - [double]$diskFree
+
+#write-host "diskused : $diskused zzz`ndisksize : ($diskSize)`n"
+$diskUsage = [math]::Round([double] $diskUsed / [double] $diskSize * 100,2)
+write-host $diskUsage
+
+# write-host "{"
+# write-host " `"data`":[`n"
+
+    # $line= "{ `"{#DISK_FREE}`" : `"" + $diskFree + "`",
+              # `"{#DISK_SIZE}`" : `"" + $diskSize + "`",
+              # `"{#DISK_USED}`" : `"" + $diskUsed + "`",
+              # `"{#DISK_USAGE}`" : `"" + $diskUsage + "`"}"
+    # write-host $line
+        
+# # Write our closing JSON brackets
+ # write-host
+# write-host " ]"
+# write-host "}"
+ 
