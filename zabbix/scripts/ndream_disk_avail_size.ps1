ff7c80188f2ee254b144741261569ae56f902ff1
jooho812 <kasin812@jam2in.com>
2018-11-21 10:39:15 +0900

    zabbix : modified powershell add ","

--- zabbix/scripts/ndream_disk_avail_size.ps1
+++ zabbix/scripts/ndream_disk_avail_size.ps1
@@ -1,6 +1,6 @@
 ﻿
 # Get Disk Information
-$diskInfo=wmic logicaldisk get size,freespace,caption | findstr $args[0]
+$diskInfo=wmic logicaldisk get size","freespace","caption | findstr $args[0]
 $caption,[string]$diskFree,[string]$diskSize=$diskInfo -split '\s+'
 $diskSize = $diskSize.Substring(0,$diskSize.length-1)
 [double] $diskUsed = [double]$diskSize - [double]$diskFree
